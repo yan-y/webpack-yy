978d6d3485dd0e392dbfc6d7402e19de28822fcc
yan-y <yannn_y@163.com>
2019-08-28 13:30:52 +0800

    sourceMap

---
+++ demo03/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+// HtmlWebpackPlugin 会在打包结束后，自动生成一个html文件，并把打包生成的js自动引入到这个html文件中
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+// plugin 可以在 webpack 运行到某个时刻的时候，帮你做一些事情
+
+module.exports = {
+    mode: 'development',
+    devtool: 'cheap-module-eval-source-map', // 如果开发环境要使用sourceMap, 打包速度最快，最详细
+    // devtool: 'cheap-module-source-map', // 如果生产环境要使用sourceMap, 可使用此配置
+    entry: {
+        main: './src/index.js'
+    },
+    module: {
+        rules: [{
+            test: /\.(jpe?g|png|gif)$/,
+            use: {
+                loader: 'url-loader',
+                options: { // placeholder 占位符
+                    name: '[name].[ext]',
+                    outputPath: 'images/',
+					limit: 10240
+                }
+            }
+        }, {
+            test: /\.(eot|ttf|svg|woff)$/,
+            use: {
+                loader: 'file-loader'
+            }
+        }, {
+            test: /\.scss$/,
+            use: [ // loader的执行顺序永远是从下到上，从右到左
+                'style-loader',
+                {
+                    loader: 'css-loader',
+                    options: {
+                        importLoaders: 2 // 此属性代表如果页面含有import引入的css文件，也会先执行postcss-loader、sass-loader
+                    }
+                },
+                'sass-loader',
+                'postcss-loader'
+            ]
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html' // 以 index 为模板生成
+        }),
+        // new CleanWebpackPlugin(['dist']) 这是一次错误写法，新版本写法如下
+        new CleanWebpackPlugin()
+    ],
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    }
+}
