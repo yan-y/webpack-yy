c31d934a14adaaed6eb6987e3d7c4719a696e90c
yan-y <yannn_y@163.com>
2019-09-21 22:13:32 +0800

    update webpack 打包优化

--- demo21/src/index.js
+++ demo21/src/index.js
@@ -5,6 +5,9 @@
  * 2. 在尽可能少的模块上应用 Loader ( 减少不必要的代码分析 )
  * 3. Plugin 尽可能精简并确保可靠
  * 4. resolve 参数合理配置
+ * 5. 控制包文件大小
+ * 6. thread-loader, parallel-webpack, happypack 多进程打包(根据项目复杂程度进行合理使用)
+ * 7. 合理使用 sourceMap
  */
 
 import React, { Component } from 'react';
